7811b664d9c0c060111d1424e19dd906e476b648
Takumi Karibe <takumi.k.5610@gmail.com>
2019-03-06 02:47:28 +0900

    fix print logic

--- requester/githubResponse.go
+++ requester/githubResponse.go
@@ -25,19 +25,32 @@ type BuiltBy struct {
 func (r *GitHubResponse) JSONPrint() {
 	fmt.Println("[")
 	for i := 0; i < r.length; i++ {
-		fmt.Println("{")
-		fmt.Printf("  developer: \"%s\",\n", r.developers[i])
-		fmt.Printf("  name: \"%s\",\n", r.names[i])
-		fmt.Printf("  url: \"%s\",\n", githubURL+r.URLs[i])
-		fmt.Printf("  description: \"%s\",\n", r.descriptions[i])
-		fmt.Printf("  language: \"%s\",\n", r.languages[i])
-		fmt.Printf("  sumStars: %d,\n", r.stars[i])
-		fmt.Printf("  forks: %d,\n", r.forks[i])
-		fmt.Printf("  ranged: {\n")
-		fmt.Printf("    type: \"%s\",\n", r.rangeType.QueryString())
-		fmt.Printf("    stars: %d\n", r.rangeStar[i])
-		fmt.Printf("  }\n")
-		fmt.Print("}")
+		fmt.Println("  {")
+		fmt.Printf("    developer: \"%s\",\n", r.developers[i])
+		fmt.Printf("    name: \"%s\",\n", r.names[i])
+		fmt.Printf("    url: \"%s\",\n", githubURL+r.URLs[i])
+		fmt.Printf("    description: \"%s\",\n", r.descriptions[i])
+		fmt.Printf("    language: \"%s\",\n", r.languages[i])
+		fmt.Printf("    sumStars: %d,\n", r.stars[i])
+		fmt.Printf("    forks: %d,\n", r.forks[i])
+		fmt.Printf("    builtBy: [\n")
+		for j := 0; j < len(r.builtBy[i]); j++ {
+			fmt.Printf("      {\n")
+			fmt.Printf("        name: \"%s\",\n", r.builtBy[i][j].name)
+			fmt.Printf("        url: %s\n", r.builtBy[i][j].url)
+			fmt.Printf("      }")
+			if j < 4 {
+				fmt.Print(",\n")
+			} else {
+				fmt.Println()
+			}
+		}
+		fmt.Printf("    ],\n")
+		fmt.Printf("    ranged: {\n")
+		fmt.Printf("      type: \"%s\",\n", r.rangeType.QueryString())
+		fmt.Printf("      stars: %d\n", r.rangeStar[i])
+		fmt.Printf("    }\n")
+		fmt.Print("  }")
 		if i < r.length-1 {
 			fmt.Print(",\n")
 		} else {
@@ -59,6 +72,11 @@ func (r *GitHubResponse) CLIPrint() {
 		fmt.Printf("  [language] %s\n", r.languages[i])
 		fmt.Printf("  [sumStars] %d\n", r.stars[i])
 		fmt.Printf("  [forks] %d\n", r.forks[i])
+		fmt.Printf("  [builtBy]\n")
+		for j := 0; j < len(r.builtBy[i]); j++ {
+			fmt.Printf("    [name] %s\n", r.builtBy[i][j].name)
+			fmt.Printf("    [url] %s\n", r.builtBy[i][j].url)
+		}
 		fmt.Printf("  [trend] %d stars %s\n", r.rangeStar[i], r.rangeType.QueryString())
 		fmt.Println()
 	}
