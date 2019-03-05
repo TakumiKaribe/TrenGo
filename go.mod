69a5335db5a1affae6c62f97537ebbfb6247ae08
Takumi Karibe <takumi.k.5610@gmail.com>
2019-03-06 00:47:36 +0900

    refactoring

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/TakumiKaribe/trengo
 
 require (
-	github.com/PuerkitoBio/goquery v1.5.0 // indirect
+	github.com/PuerkitoBio/goquery v1.5.0
 	github.com/maka-nai/trengo v0.0.0-20190217110952-47478fb19f31
 )
--- main.go
+++ main.go
@@ -19,33 +19,26 @@ func main() {
 	flag.StringVar(&lang, "l", "", "language name")
 	flag.BoolVar(&w, "w", false, "weekly search")
 	flag.BoolVar(&m, "m", false, "monthly search")
-	flag.BoolVar(&g, "g", true, "GitHub search")
 	flag.BoolVar(&j, "j", false, "json format")
 
 	flag.Parse()
-	checkSearchRange()
-
-	var githubResponse requester.GitHubResponse
-	if g {
-		rangeType := requester.Daily
-		if w {
-			rangeType = requester.Weekly
-		}
-		if m {
-			rangeType = requester.Monthly
-		}
-
-		githubResponse = requester.ParseGitHub(rangeType, lang)
-		if j {
-			githubResponse.JSONPrint()
-		} else {
-			githubResponse.CLIPrint()
-		}
-	}
-}
-
-func checkSearchRange() {
 	if w && m {
 		log.Fatalf("too many parameters.")
+	}
+
+	var githubResponse requester.GitHubResponse
+	rangeType := requester.Daily
+	if w {
+		rangeType = requester.Weekly
+	}
+	if m {
+		rangeType = requester.Monthly
+	}
+
+	githubResponse = requester.ParseGitHub(rangeType, lang)
+	if j {
+		githubResponse.JSONPrint()
+	} else {
+		githubResponse.CLIPrint()
 	}
 }
